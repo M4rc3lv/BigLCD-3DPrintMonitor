490acfd19b617fc080b85990b7e9a9902430c29e
Marcel V <19297663+M4rc3lv@users.noreply.github.com>
2023-08-10 12:31:27 +0200

    Bugfix met website-aanroep en urlencode's

--- MonitormetBigLCD/MonitormetBigLCD.ino
+++ MonitormetBigLCD/MonitormetBigLCD.ino
@@ -1,108 +1,120 @@
-#include "Config.h"
-#include "LiquidCrystal_I2C.h"
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ArduinoJson.h> 
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h> // Library van Benoit BLANCHON
 #include "Printer.h"
+#include <UrlEncode.h>
+#include "Config.h"
 
-ESP8266WiFiMulti D1Mini;
-LiquidCrystal_I2C LCD(0x27, 20,4);
-/* Aansluiten I2C LCD
-GND GND
-VCC 5V
-SDA D2
-SCL D1 */
-
-const char *Printers[]={
- PRINTER1,
- PRINTER2,
- PRINTER3,
- PRINTER4
-};
-
-Printer *p[NUMPRINTERS];
-void Cls(const char *txt=NULL);
-
-void setup() {
- Serial.begin(115200);
- LCD.begin(5,4); // I2C LCD aangesloten op D1 (SCL) and D2 (SDA)
- LCD.init();
- LCD.backlight();
-
- Cls("Wifi starten...");
- WiFi.mode(WIFI_STA);
- D1Mini.addAP(WIFI_SSID, WIFI_PASSWORD); 
- Cls("Wifi OK!"); 
- delay(500); 
-
- for(int i=0; i<NUMPRINTERS; i++) {
-  p[i]=new Printer(Printers[i*3], Printers[i*3+1], Printers[i*3+2]);
- }
+Printer::Printer(String Naam, String IPAdres, String APIKey) 
+:_JobJson(400),_PrinterJson(600)
+ { 
+ _Naam=Naam;
+ _IPAdres=IPAdres;
+ _APIKey=APIKey; 
+ IsMonitoring=IsImageUploaded=false;
 }
 
-void loop() {
- static long BacklightTimer=millis();
- if(D1Mini.run()== WL_CONNECTED){   
-  int nAantalPrinten=0;
-  for(int i=0; i<NUMPRINTERS; i++) {
-   p[i]->Refresh();
-   ClrLine(i);
-   LCD.setCursor(0,i);
-   if(p[i]->IsPrinting()) {  
-    nAantalPrinten++;
-    LCD.backlight();
-    p[i]->IsMonitoring=true;  
-    int procent=p[i]->ProcentComplete();
-    String sProcent(procent);
-    if(procent<100) sProcent+="%"; else sProcent="100";
-    LCD.printf("%3d%c %-3s %-11s", p[i]->Temperatuur(), 223/*Gradensymbooltje*/, sProcent.c_str(), p[i]->Filenaam(11).c_str());     
-
-    if(!p[i]->IsImageUploaded) {
-     p[i]->GetImageFromPrinter();
-     p[i]->UploadImgToServer();
-     p[i]->IsImageUploaded=true;
-    }
-   }
-   else {
-    if(p[i]->IsMonitoring) {
-     p[i]->SendPrintReadyEmail();
-     LCD.printf("Klaar: %s",p[i]->Filenaam(13).c_str());
-     p[i]->IsMonitoring=false;
-    }    
-    else if(p[i]->Status().indexOf("Paused")==0) LCD.print("Gepauzeerd          ");
-    else {
-     String f=p[i]->Filenaam(13);
-     if(f!="null" && f.length()>0)
-      LCD.printf("Klaar: %s",f.c_str());
-     else
-      LCD.print("                    ");
-    }
-    p[i]->IsImageUploaded=false;
-   }
-  }
-  // Als geen enkele printer iets doet, zet dan het LCD na 3 minuten uit
-  if(nAantalPrinten<=0 && millis()-BacklightTimer>180000)
-   LCD.noBacklight();
-  else {
-   if(nAantalPrinten>0) BacklightTimer=millis();
-   LCD.backlight();
-  }
- }
- else {
-  LCD.clear();
-  LCD.print("Geen netwerk!");
-  LCD.backlight();
- }
- delay(10000);
+void Printer::Refresh() { 
+ WiFiClient Wifi;
+ HTTPClient http;  
+ http.begin(Wifi,_IPAdres+String("/api/job")); 
+ http.addHeader("X-Api-Key", _APIKey);        
+ int httpCode = http.GET();
+ if(httpCode > 0) {     
+  String payload = http.getString();  
+  deserializeJson(_JobJson, payload);
+  Serial.print("JOB:");Serial.println(payload);
+ } 
+ http.end();
+ http.begin(Wifi,_IPAdres+String("/api/printer")); 
+ http.addHeader("X-Api-Key", _APIKey);        
+ httpCode = http.GET();
+ if(httpCode > 0) {     
+  String payload = http.getString();  
+  deserializeJson(_PrinterJson, payload);
+  Serial.print(String("Printer ")+String(_Naam)+String(": "));
+  Serial.println(payload);
+ } 
+ http.end();
 }
 
-void ClrLine(int Line) {
- LCD.setCursor(0,Line);
- LCD.print("                    ");
+String Printer::Status() {
+ return _JobJson["state"].as<String>();
 }
 
-void Cls(const char *txt /*=NULL*/) {
- LCD.clear();
- LCD.setCursor(0,0);
- if(txt) LCD.print(txt);
+int Printer::ProcentComplete() {
+ return (int)round(_JobJson["progress"]["completion"].as<float>()*100.0);
+}
+
+bool Printer::IsPrinting() {
+ return Status().indexOf("Printing")>=0;
+}
+
+int Printer::Temperatuur() {
+ float t =_PrinterJson["temperature"]["tool0"]["actual"].as<float>();
+ Serial.println(t);
+ return (int)round(t);
+}
+
+String Printer::Filenaam(int maxlen/*=0*/) {
+ String f=_JobJson["job"]["file"]["name"].as<String>(); 
+ if(f.length()>0 && f!="null")
+  _LastFileName=f;
+
+ f=_LastFileName; 
+ int dotIndex=f.lastIndexOf('.');
+ if(dotIndex >= 0)
+  f = f.substring(0, dotIndex); 
+ if(maxlen>0 && f.length()>maxlen)
+  f=f.substring(0,maxlen);
+ return f;
+}
+
+void Printer::SendPrintReadyEmail() {
+ // sendprintcompletemail.php?printernaam=MK3&bestandsnaam=bestandje&imagenaam=Plaatje
+ WiFiClient Wifi;  
+ HTTPClient http;
+ String url=String(MY_WEBSITE)+
+  String("sendprintcompletemail.php?printernaam=")+urlEncode(_Naam)+
+  String("&bestandsnaam=")+urlEncode(Filenaam(100))+
+  String("&imagenaam=")+urlEncode(_Naam)+urlEncode(".png");
+ Serial.println(url); 
+ http.begin(Wifi,url); 
+ int httpCode = http.GET();
+ if(httpCode>0) {
+  Serial.print("E-mail verstuurd!");  
+ }
+ else Serial.printf("Code:%d\1\n",httpCode); 
+ http.end(); 
+}
+
+void Printer::GetImageFromPrinter() {
+ String PicFileName=_JobJson["job"]["file"]["path"].as<String>(); // /usb/TRECHT~3.GCO ==>  thumb/l/usb/TRECHT~2.GCO
+ Serial.printf("Imgfile: %s\r\n",PicFileName.c_str());
+ WiFiClient Wifi;  
+ HTTPClient http;
+ http.begin(Wifi,_IPAdres+"/thumb/l"+PicFileName);   
+ http.addHeader("X-Api-Key", _APIKey);  
+ int httpCode = http.GET();
+ if(httpCode>0) {
+  _Image=http.getString(); 
+  Serial.printf("Inlezen gelukt, lengte=%ld\r\n",_Image.length());  
+ }
+ http.end(); 
+}
+
+void Printer::UploadImgToServer() {
+ Serial.printf("Verstuur naar mijn server %d\r\n",_Image.length());
+ WiFiClient c;      
+ HTTPClient http2;
+ String url=String(MY_WEBSITE)+String("upload.php?")+String("naam=")+urlEncode(_Naam+".png");
+ Serial.println(url);
+ http2.begin(c,url);  
+ http2.addHeader("Content-Type","application/octet-stream");
+ Serial.println("0");
+ //http2.POST((uint8_t*)pic.c_str(),pic.length());
+ http2.sendRequest("PUT",(uint8_t*)_Image.c_str(),_Image.length());
+ Serial.println("1");
+ http2.end();
+ 
+ _Image="";
 }
